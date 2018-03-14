fde170ce6f4f58ee17b63d06d1fba51287835762
ViniciusBVilar <viniciusb.vilar@hotmail.com>
2018-03-14 00:19:43 -0300

    Install prop-types and babel-eslint to solve eslint issues

--- src/components/bookshelf.component.js
+++ src/components/bookshelf.component.js
@@ -1,38 +1,28 @@
 import React, { Component } from 'react';
-import Shelf from './shelf.component';
-import * as BooksAPI from '../data-source/BooksAPI';
-import { Link } from 'react-router-dom';
+import Book from './book.component';
+import PropTypes from 'prop-types';
 import '../assets/styles/bookshelf.css';
 import '../App.css';
 
 class Bookshelf extends Component {
 
-  state = {
-    shelfs: {
-      currentlyReading: [],
-      wantToRead: [],
-      read: []
-    }
-  }
-
-  shelfs = {
-    currentlyReading: [],
-    wantToRead: [],
-    read: []
-  }
-
-  componentDidMount() {
-    BooksAPI.getAll()
-      .then((books) => books.map((book) => this.shelfs[book.shelf].push(book)))
-      .then(() => this.setState({ shelfs: this.shelfs }));
-  }
+  static propTypes = {
+    shelf: PropTypes.array.isRequired,
+    shelfbooks: PropTypes.array.isRequired,
+  };
 
   render() {
+    console.log('aaaaaaasss', this.props.shelfbooks);
     return (
-      <div>
-        <div className="bookshelf-top">
-          <div className="bookshelf-title">
-            <h1>Read</h1>
+      <div className="bookshelf">
+        <h1 className="bookshelf-title">{this.props.shelf}</h1>
+        <div className="bookshelf-books">
+          <div className="bookshelf-grid">
+            <li>
+              {/* {this.props.shelfbooks.map((book) => ( */}
+              <Book />
+              {/* ))} */}
+            </li>
           </div>
           <Link
             to="/search"
