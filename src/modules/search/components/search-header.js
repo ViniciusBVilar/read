8bd1ac7730a9e9b51e2c4eae6b9f484b0ffac8b7
ViniciusBVilar <viniciusb.vilar@hotmail.com>
2018-04-04 01:54:39 -0300

    Fix form structure

--- src/modules/search/components/search-header.js
+++ src/modules/search/components/search-header.js
@@ -36,36 +36,36 @@ class SearchHeader extends React.Component {
 
   handleSubmit = (event) => {
     this.props.search(document.getElementById('categoryInput').value);
-    //TODO: fix to prevent default form action. The following code is not working
     event.preventDefault();
-    return false;
   }
 
   render() {
     const { showingCategories, query } = this.state;
     return (
-      <div className="search-books-bar">
+      <div className="search-bar">
         <Link className="back-search" to="/">Back</Link>
-        <form autoComplete="off" onSubmit={this.handleSubmit}>
-          <div className="autocomplete">
-            <input id="categoryInput" type="text" name="searchField"
-              placeholder="Search books by category" autoFocus
-              value={query}
-              onChange={event => this.updateQuery(event.target.value)} />
+        <div className="search-bar-container">
+          <form autoComplete="off" onSubmit={this.handleSubmit}>
+            <div className="autocomplete">
+              <input id="categoryInput" type="text" name="searchField"
+                placeholder="Search books by category" autoFocus
+                value={query}
+                onChange={event => this.updateQuery(event.target.value)} />
+              <input id="autoCompleteSubmit" type="submit" className="search" name="searchFieldSubmit"
+                alt="search"/>
+            </div>
+          </form>
+          <p className="auto-complete-title">{showingCategories.length === CATEGORIES.length ?
+            'All categories:' : 'Filtered categories:'}</p>
+          <div className="auto-complete-container">
+            <select onChange={(event) => this.props.search(event.target.value)}>
+              {showingCategories.map((category, index) => (
+                <option key={index}>
+                  {category}
+                </option>
+              ))}
+            </select>
           </div>
-        </form>
-        <button id="autoCompleteSubmit" type="submit" className="search" name="searchFieldSubmit"
-          alt="search" onClick={() => this.props.search(document.getElementById('categoryInput').value)} />
-        <p className="auto-complete-title">{showingCategories.length === CATEGORIES.length ?
-          'All categories:' : 'Filtered categories:'}</p>
-        <div className="auto-complete-container">
-          <select onChange={(event) => this.props.search(event.target.value)}>
-            {showingCategories.map((category, index) => (
-              <option key={index}>
-                {category}
-              </option>
-            ))}
-          </select>
         </div>
       </div>
     );
